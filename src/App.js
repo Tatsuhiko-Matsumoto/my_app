738f069490f8a77f08b1b25a26d5189e40fc9398
Ubuntu <ubuntu@ip-172-31-22-242.us-west-2.compute.internal>
2021-11-10 02:47:12 +0000

    changes for V8

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 // import { withAuthenticator } from '@aws-amplify/ui-react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { Auth } from 'aws-amplify';
 
 function App() {
   Auth.currentCredentials().then(console.log);
   return (
     <div className="App">
       <header className="App-header">
-	<h1>Hello from V7</h1>
+	<h1>Hello from V8</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
